59d90fe2566c8d298a3f020d6ec53589cd079bc4
Offizil <fredoffizil@gmail.com>
2025-05-04 19:28:36 +0100

    added course home page

---
+++ scripts/getdates.js
@@ -0,0 +1,20 @@
+// -----------SCRIPT FOR LAST MODIFIES TIME---------------
+  // Get the current date and time
+  const lastModifiedString = new Date(document.lastModified);
+
+  // Format the date as a string
+  const formattedDate = lastModifiedString.toDateString();
+
+  // Format the time as a string
+  const formattedTime = lastModifiedString.toLocaleTimeString();
+
+  // Get the element with the id "lastModified"
+  const lastModifiedElement = document.getElementById("lastModified");
+
+  // Update the content of the element
+  lastModifiedElement.textContent = `Last modified: ${formattedDate} at ${formattedTime}`;
+
+
+
+
+  
