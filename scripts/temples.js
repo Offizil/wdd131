3e9999c0f1e5dd6b8bdbe7768b2593cedd476309
Offizil <fredoffizil@gmail.com>
2025-05-17 23:45:31 +0100

    week02

---
+++ scripts/temples.js
@@ -0,0 +1,24 @@
+// const hamButton = document.querySelector('#menu');
+// const navigation = document.querySelector('.navigation');
+
+// hamButton.addEventListener('click', () => {
+// 	navigation.classList.toggle('show');
+// 	hamButton.classList.toggle('show');
+// });
+
+
+
+  const btn  = document.getElementById('menu');
+  const nav  = document.querySelector('.navigation');
+
+  btn.addEventListener('click', () => {
+    nav.classList.toggle('show');
+
+    const expanded = btn.getAttribute('aria-expanded') === 'true';
+    btn.setAttribute('aria-expanded', !expanded);
+
+    // swap ☰ ↔ X
+    btn.textContent = expanded ? '☰' : '✖';
+    btn.setAttribute('aria-label', expanded ? 'Open menu' : 'Close menu');
+  });
+
---
+++ week02/scripts/script.js
@@ -0,0 +1,44 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+let error = document.querySelector("#error-msg");
+
+
+input.focus();
+
+button.addEventListener("click", () => {
+    if (input.value != "")
+    {
+        error.textContent = "";   
+        const li = document.createElement("li");
+        const delBtn = document.createElement("button");
+
+        li.textContent = input.value;
+        delBtn.textContent = "❌";
+
+        li.append(delBtn);
+        list.appendChild(li);
+        input.focus();
+        input.value="";
+
+
+
+        delBtn.addEventListener("click", function(){
+            list.removeChild(li);
+            input.focus();
+
+            input.value="";
+        })
+
+
+
+    }
+    else {
+        // message.innerHTML = `Please enter a chapter.`;
+        input.focus();
+        error.textContent = "❗❗ You must enter chapter ❗❗";
+
+      }
+
+
+})
