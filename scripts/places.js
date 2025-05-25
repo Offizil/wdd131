6c3612dd7dacbe8b7e80472ee6cea3fa751cceb4
Offizil <fredoffizil@gmail.com>
2025-05-25 20:15:09 +0100

    fixed error

--- scripts/places.js
+++ scripts/places.js
@@ -6,13 +6,12 @@ const winchill = document.querySelector("#windchill");
 function Windchill() {
    
         if (temp <= 50 || velocity > 4.8) {
-        let Ta = temperature;
         // Calculate
         let T = 35.74 + (0.6215 * Ta) - (35.75 * velocity) ** 0.16 + (0.4275 * Ta) ** 0.16;
         winchill.textContent = `Wind Chill: ${T.toFixed(2)} ℉ `;
-    } else {
-        winchill.textContent = "N/A";
-    }
+        } else {
+            winchill.textContent = "N/A";
+        }
 }
 
 
